9b1fc49d7719af5d67179b75073be91de1d307bb
Shana Slavin <shanaslavin@gmail.com>
2019-04-05 15:19:36 -0400

    done with CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new  
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save(post_params)
+      redirect_to @post
+    else 
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to post_path
+  end
+
+  private
+
+  def post_params 
+    params.require(:post).permit(:title, :detail, :avatar)
+  end
+end
